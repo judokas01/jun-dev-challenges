45b4cced057423f97e58868c10f12d5f329e4c81
Vera <zdrazilova.ver@gmail.com>
2025-11-19 13:26:45 +0100

    jest tests 2 object matching

--- src/testing/2-object-matching.spec.ts
+++ src/testing/2-object-matching.spec.ts
@@ -20,13 +20,38 @@ const createStaticAddress = (isCompany = false): Address => ({
 
 describe('Object matching of createStaticAddress', () => {
     describe('Match to object', () => {
-        it('should create a address, when company the isCompany is true', () => {})
-        it('should create a address, when company the isCompany is false', () => {})
+        it('should create a address, when company the isCompany is true', () => {
+            const address = createStaticAddress(true)
+            expect(address).toEqual({
+                street: 'Dlouha trida 1',
+                city: 'Prague',
+                country: 'Czech Republic',
+                zipCode: '11000',
+                name: 'Prokop Buben',
+                company: 'Alza',
+            })
+        })
+        it('should create a address, when company the isCompany is false', () => {
+            const address = createStaticAddress(false)
+            expect(address).toEqual({
+                street: 'Dlouha trida 1',
+                city: 'Prague',
+                country: 'Czech Republic',
+                zipCode: '11000',
+                name: 'Prokop Buben',
+            })
+        })
     })
 
     describe('Match to snapshot', () => {
-        it('should create a address, when company the isCompany is true', () => {})
-        it('should create a address, when company the isCompany is false', () => {})
+        it('should create a address, when company the isCompany is true', () => {
+            const address = createStaticAddress(true)
+            expect(address).toMatchSnapshot()
+        })
+        it('should create a address, when company the isCompany is false', () => {
+            const address = createStaticAddress(false)
+            expect(address).toMatchSnapshot()
+        })
     })
 })
 
@@ -41,13 +66,34 @@ const createPartiallyStaticAddress = (isCompany = false): Address => ({
 
 describe('Object matching of createPartiallyStaticAddress', () => {
     describe('Match to object', () => {
-        it('should create a address, when company the isCompany is true', () => {})
-        it('should create a address, when company the isCompany is false', () => {})
-    })
+        it('should create a address, when company the isCompany is true', () => {
+            const address = createPartiallyStaticAddress(true)
+            expect(address).toMatchObject({
+                city: 'Prague',
+                country: 'Czech Republic',
+                zipCode: '11000',
+                company: 'Alza',
+            })
+        })
+        it('should create a address, when company the isCompany is false', () => {
+            const address = createPartiallyStaticAddress(false)
+            expect(address).toMatchObject({
+                city: 'Prague',
+                country: 'Czech Republic',
+                zipCode: '11000',
+            })
+        })
 
-    describe('Match to snapshot', () => {
-        it('should create a address, when company the isCompany is true', () => {})
-        it('should create a address, when company the isCompany is false', () => {})
+        describe('Match to snapshot', () => {
+            it('should create a address, when company the isCompany is true', () => {
+                const address = createPartiallyStaticAddress(true)
+                expect(address).toMatchSnapshot()
+            })
+            it('should create a address, when company the isCompany is false', () => {
+                const address = createPartiallyStaticAddress(false)
+                expect(address).toMatchSnapshot()
+            })
+        })
     })
 })
 
@@ -62,12 +108,28 @@ const createAllRandomAddress = (isCompany = false): Address => ({
 
 describe('Object matching of createAllRandomAddress', () => {
     describe('Match to object', () => {
-        it('should create a address, when company the isCompany is true', () => {})
-        it('should create a address, when company the isCompany is false', () => {})
-    })
+        it('should create a address, when company the isCompany is true', () => {
+            const address = createAllRandomAddress(true)
+            expect(address).toMatchObject({
+                company: expect.any(String),
+            })
+        })
+        it('should create a address, when company the isCompany is false', () => {
+            const address = createAllRandomAddress(false)
+            expect(address).toMatchObject({
+                company: undefined,
+            })
+        })
 
-    describe('Match to snapshot', () => {
-        it('should create a address, when company the isCompany is true', () => {})
-        it('should create a address, when company the isCompany is false', () => {})
+        describe('Match to snapshot', () => {
+            it('should create a address, when company the isCompany is true', () => {
+                const address = createAllRandomAddress(true)
+                expect(address).toMatchSnapshot()
+            })
+            it('should create a address, when company the isCompany is false', () => {
+                const address = createAllRandomAddress(false)
+                expect(address).toMatchSnapshot()
+            })
+        })
     })
 })
